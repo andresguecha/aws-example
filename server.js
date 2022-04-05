3dc632c04cb4fb109c0d7ed11a9e9a5d394adfd4
andresguecha <andresg1310@gmail.com>
2022-04-05 07:02:21 -0500

    tercer cambio - server html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { get } = require('http')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Word!! with express'))
+app.get('/', (req, res) => res.send('<h1>Hello World express html</h1>'))
 
 app.listen(3000)
 console.log('server is running 3000')
